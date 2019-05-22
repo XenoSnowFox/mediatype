44f4f2ae3d8653d5492886fd63a9dbe2dcbab7e2
Aaron Brooks <xenosnowfox@gmail.com>
2019-05-22 14:26:32 +1000

    progress commit

--- src/main/java/com/xenosnowfox/mediatype/MediaType.java
+++ src/main/java/com/xenosnowfox/mediatype/MediaType.java
@@ -18,9 +18,9 @@ public class MediaType {
     private String type;
 
     /**
-     * Registration tree the subtype belongs to.
+     * Registration tree the subtype belongs to. Defaults to the standards tree.
      */
-    private RegistrationTree registrationTree;
+    private RegistrationTree registrationTree = RegistrationTree.STANDARDS;
 
     /**
      * Sub type text string.
@@ -43,10 +43,60 @@ public class MediaType {
      * @param mediaType
      *      The string to be parsed into a media type.
      * @throws MediaTypeSyntaxException
-     *      If the given string violates RFC ????.
+     *      If the given string violates RFC standards.
      */
     public MediaType(final String mediaType) throws MediaTypeSyntaxException {
+        String[] parameters = mediaType.split(";");
 
+        // item 0 is the media type data
+        String[] parts = parameters[0].split("/", 2);
+        if (parts.length != 2) {
+            throw new MediaTypeSyntaxException(mediaType, "Invalid Syntax", 0);
+        }
+
+        // type
+        this.type = parts[0].trim().toLowerCase();
+        if (this.type.length() == 0) {
+            throw new MediaTypeSyntaxException(mediaType, "No top-level type information provided.", 0);
+        }
+
+        // extract the sub-type for parsing
+        this.subType = parts[1].trim().toLowerCase();
+
+        // determine the suffix
+        parts = this.subType.split("\\+");
+        if (parts.length > 2) {
+            throw new MediaTypeSyntaxException(mediaType, "Too many suffixes provided");
+        } else if (parts.length == 2) {
+            this.subType = parts[0].trim();
+            this.suffix = parts[1].trim();
+        }
+
+        // determine the registration tree and sub type
+        String[] subTypeParts = this.subType.split("\\.", 2);
+        if (subTypeParts.length > 1) {
+            RegistrationTree tree = RegistrationTree.withPrefix(subTypeParts[0] + ".");
+            if (tree != null) {
+                this.registrationTree = tree;
+                this.subType = subTypeParts[1];
+            }
+        }
+
+        // loop over remaining parameters
+        for(int i = 1; i < parameters.length; i++) {
+
+            parts = parameters[i].split("=", 2);
+            if (parts.length != 2) {
+                throw new MediaTypeSyntaxException(mediaType, "Invalid parameter value");
+            }
+
+            // parse the value
+            String value = parts[1].trim();
+            if (value.startsWith("\"") && value.endsWith("\"")) {
+                value = value.substring(1, value.length() - 1);
+            }
+            this.parameterMap.put(parts[0].trim().toLowerCase(), value);
+        }
     }
 
     /**
@@ -56,11 +106,9 @@ public class MediaType {
      *      The top level type.
      * @param subType
      *      The specific sub type.
-     * @throws MediaTypeSyntaxException
-     *      If the given string violates RFC ????.
      */
-    public MediaType(final String type, final String subType) throws MediaTypeSyntaxException {
-
+    public MediaType(final String type, final String subType) {
+        this(type, subType, null);
     }
 
     /**
@@ -72,11 +120,19 @@ public class MediaType {
      *      The specific sub type.
      * @param suffix
      *      Addition suffix information
-     * @throws MediaTypeSyntaxException
-     *      If the given string violates RFC ????.
      */
-    public MediaType(final String type, final String subType, final String suffix) throws MediaTypeSyntaxException {
+    public MediaType(final String type, final String subType, final String suffix) {
+        this(RegistrationTree.STANDARDS, type, subType, suffix);
 
+        // determine the registration tree and sub type
+        String[] subTypeParts = subType.split("\\.", 2);
+        if (subTypeParts.length > 1) {
+            RegistrationTree tree = RegistrationTree.withPrefix(subTypeParts[0] + ".");
+            if (tree != null) {
+                this.registrationTree = tree;
+                this.subType = subTypeParts[1];
+            }
+        }
     }
 
     /**
@@ -88,11 +144,9 @@ public class MediaType {
      *      The top level type.
      * @param subType
      *      The specific sub type.
-     * @throws MediaTypeSyntaxException
-     *      If the given string violates RFC ????.
      */
-    public MediaType(final RegistrationTree registrationTree, final String type, final String subType) throws MediaTypeSyntaxException {
-
+    public MediaType(final RegistrationTree registrationTree, final String type, final String subType) {
+        this(registrationTree, type, subType, null);
     }
 
     /**
@@ -106,18 +160,123 @@ public class MediaType {
      *      The specific sub type.
      * @param suffix
      *      Addition suffix information
-     * @throws MediaTypeSyntaxException
-     *      If the given string violates RFC 7231.
      */
-    public MediaType(final RegistrationTree registrationTree, final String type, final String subType, final String suffix) throws MediaTypeSyntaxException {
+    public MediaType(final RegistrationTree registrationTree, final String type, final String subType, final String suffix) {
         this.registrationTree = registrationTree;
         this.type = type;
         this.subType = subType;
         this.suffix = suffix;
     }
 
+    /**
+     * Obtains the MediaType's top level type information.
+     *
+     * @return
+     *      Top level type.
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Obtains the MediaType's sub type information.
+     *
+     * @return
+     *      Secondary level sub type.
+     */
+    public String getSubType() {
+        return subType;
+    }
+
+    /**
+     * Obtains the MediaType's suffix information.
+     *
+     * @return
+     *      Suffix or {@code null} if no suffix is defined.
+     */
+    public String getSuffix() {
+        return suffix;
+    }
+
+    /**
+     * Obtains the registration tree the suffix is part of.
+     *
+     * @return
+     *      Registration tree.
+     */
+    public RegistrationTree getRegistrationTree() {
+        return registrationTree;
+    }
+
+    /**
+     * Adds the specified parameter to the Media Type, overriding any existing value.
+     *
+     * @param name
+     *      name of the parameter
+     * @param value
+     *      value to store against the parameter
+     * @throws NullPointerException
+     *      if either the name or value arguments are {@code null}
+     * @throws IllegalArgumentException
+     *      if the name argument is blank or empty
+     */
     public void putParameter(final String name, final String value) {
-        this.parameterMap.put(name, value);
+        if (name == null) {
+            throw new NullPointerException("Parameter name cannot be null.");
+        }
+
+        if (name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Parameter name not provided.");
+        }
+
+        if (value == null) {
+            throw new NullPointerException("Parameter value cannot be null.");
+        }
+
+        this.parameterMap.put(name.trim().toLowerCase(), value);
+    }
+
+    /**
+     * Returns the specified parameter value.
+     *
+     * @param name
+     *      Name of the parameter to return.
+     * @return
+     *      Parameter value or {@code null} if the parameter has not been defined
+     */
+    public String getParameter(final String name) {
+        return this.parameterMap.get(name.trim().toLowerCase());
+    }
+
+    @Override
+    public boolean equals(final Object object) {
+        if (object == null) {
+            return false;
+        }
+
+        MediaType other;
+        if (object instanceof MediaType) {
+            other = (MediaType) object;
+        } else if(object instanceof String) {
+            try {
+                other = new MediaType((String) object);
+            } catch (Exception ex) {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+        return (
+                other.type.equalsIgnoreCase(this.type)
+                && other.subType.equalsIgnoreCase(this.subType)
+                && other.registrationTree == this.registrationTree
+                && (
+                        (other.suffix != null && other.suffix.equalsIgnoreCase(this.suffix))
+                        || (other.suffix == null && this.suffix == null)
+                )
+                && other.parameterMap.equals(this.parameterMap)
+        );
     }
 
     @Override
@@ -132,6 +291,10 @@ public class MediaType {
 
         // add parameters
         this.parameterMap.forEach((key, value) -> {
+            if (value == null) {
+                return;
+            }
+
             sb.append("; ").append(key).append("=");
 
             if (value.contains("\"")) {
--- src/test/java/com/xenosnowfox/mediatype/MediaType_UnitTest.java
+++ src/test/java/com/xenosnowfox/mediatype/MediaType_UnitTest.java
@@ -1,14 +1,160 @@
 package com.xenosnowfox.mediatype;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MediaType_UnitTest {
     @Test
     public void run() throws Exception {
-        MediaType mediaType = new MediaType(RegistrationTree.VENDOR, "application", "aroflo.timers.timer", "json");
+        MediaType mediaType = new MediaType(RegistrationTree.STANDARDS, "application", "com.example", "json");
         mediaType.putParameter("version", "1");
         mediaType.putParameter("charset", "UTF-8");
 
+        mediaType = new MediaType("application/vnd.aroflo-1+json;   \r\n   version=1    ;    something=\"other\"");
+
         System.out.println(mediaType.toString());
+        System.out.println(mediaType.getType());
+        System.out.println(mediaType.getSubType());
+        System.out.println(mediaType.getRegistrationTree());
+        System.out.println(mediaType.getSuffix());
+        System.out.println(mediaType.getParameter("version"));
+        System.out.println(mediaType.getParameter("something"));
+    }
+
+    @Test
+    public void testMethod_equals_exactMatch() throws Exception {
+
+        MediaType mediaType1 = new MediaType(RegistrationTree.STANDARDS, "application", "com.example", "json");
+        mediaType1.putParameter("version", "1");
+        mediaType1.putParameter("charset", "UTF-8");
+
+        MediaType mediaType2 = new MediaType(RegistrationTree.STANDARDS, "application", "com.example", "json");
+        mediaType2.putParameter("version", "1");
+        mediaType2.putParameter("charset", "UTF-8");
+
+        Assertions.assertEquals(
+                mediaType1
+                , mediaType2
+        );
+    }
+
+    @Test
+    public void testMethod_equals_exactMatchWithNoParameters() throws Exception {
+
+        MediaType mediaType1 = new MediaType(RegistrationTree.STANDARDS, "application", "com.example", "json");
+
+        MediaType mediaType2 = new MediaType(RegistrationTree.STANDARDS, "application", "com.example", "json");
+
+        Assertions.assertEquals(
+                mediaType1
+                , mediaType2
+        );
+    }
+
+    @Test
+    public void testMethod_equals_nonMatchWithDifferentParameters() throws Exception {
+
+        MediaType mediaType1 = new MediaType(RegistrationTree.STANDARDS, "application", "com.example", "json");
+        mediaType1.putParameter("version", "1");
+
+        MediaType mediaType2 = new MediaType(RegistrationTree.STANDARDS, "application", "com.example", "json");
+        mediaType2.putParameter("version", "1");
+        mediaType2.putParameter("charset", "UTF-8");
+
+        Assertions.assertNotEquals(
+                mediaType1
+                , mediaType2
+        );
+    }
+
+    @Test
+    public void testMethod_equals_nonMatchWithSimilarParameters() throws Exception {
+
+        MediaType mediaType1 = new MediaType(RegistrationTree.STANDARDS, "application", "com.example", "json");
+        mediaType1.putParameter("version", "2");
+
+        MediaType mediaType2 = new MediaType(RegistrationTree.STANDARDS, "application", "com.example", "json");
+        mediaType2.putParameter("version", "1");
+
+        Assertions.assertNotEquals(
+                mediaType1
+                , mediaType2
+        );
+    }
+
+    @Test
+    public void testMethod_equals_nonMatchWithDifferentRegistrationTrees() throws Exception {
+
+        MediaType mediaType1 = new MediaType(RegistrationTree.UNREGISTERED, "application", "com.example", "json");
+
+        MediaType mediaType2 = new MediaType(RegistrationTree.STANDARDS, "application", "com.example", "json");
+
+        Assertions.assertNotEquals(
+                mediaType1
+                , mediaType2
+        );
+    }
+
+    @Test
+    public void testMethod_equals_nonMatchWithDifferentTypes() throws Exception {
+
+        MediaType mediaType1 = new MediaType(RegistrationTree.STANDARDS, "application", "com.example", "json");
+
+        MediaType mediaType2 = new MediaType(RegistrationTree.STANDARDS, "image", "com.example", "json");
+
+        Assertions.assertNotEquals(
+                mediaType1
+                , mediaType2
+        );
+    }
+
+    @Test
+    public void testMethod_equals_nonMatchWithDifferentSubTypes() throws Exception {
+
+        MediaType mediaType1 = new MediaType(RegistrationTree.STANDARDS, "application", "com.example-1", "json");
+
+        MediaType mediaType2 = new MediaType(RegistrationTree.STANDARDS, "application", "com.example-2", "json");
+
+        Assertions.assertNotEquals(
+                mediaType1
+                , mediaType2
+        );
+    }
+
+    @Test
+    public void testMethod_equals_nonMatchWithDifferentSuffix() throws Exception {
+
+        MediaType mediaType1 = new MediaType(RegistrationTree.STANDARDS, "application", "com.example", "json");
+
+        MediaType mediaType2 = new MediaType(RegistrationTree.STANDARDS, "application", "com.example", "xml");
+
+        Assertions.assertNotEquals(
+                mediaType1
+                , mediaType2
+        );
+    }
+
+    @Test
+    public void testConstructor_withOnlyTypeAndSubType() throws Exception {
+        MediaType mediaType = new MediaType("application", "com.example");
+
+        Assertions.assertAll(
+                () -> Assertions.assertEquals("application", mediaType.getType())
+                , () -> Assertions.assertEquals("com.example", mediaType.getSubType())
+                , () -> Assertions.assertNull(mediaType.getSuffix())
+                , () -> Assertions.assertEquals(RegistrationTree.STANDARDS, mediaType.getRegistrationTree())
+        );
+    }
+
+    @Test
+    public void testConstructor_withOnlyTypeAndVendorSubType() throws Exception {
+        MediaType mediaType = new MediaType("application", "vnd.example");
+
+        Assertions.assertAll(
+                () -> Assertions.assertEquals("application", mediaType.getType())
+                , () -> Assertions.assertEquals("example", mediaType.getSubType())
+                , () -> Assertions.assertNull(mediaType.getSuffix())
+                , () -> Assertions.assertEquals(RegistrationTree.VENDOR, mediaType.getRegistrationTree())
+        );
     }
 }
